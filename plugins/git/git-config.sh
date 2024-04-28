1858226f0348cef905128502c244e8bebb38aaae
x <y>
2024-04-28 23:24:40 +0200

    minor print change

--- plugins/git/git-config.sh
+++ plugins/git/git-config.sh
@@ -6,6 +6,7 @@ git config --global user.email $2
 username=$(git config --global user.name)
 email=$(git config --global user.email)
 
+printf "\nGit Environment\n\n"
 echo USERNAME + EMAIL updated
 echo user.name=$username
 echo user.email=$email
