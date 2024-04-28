5641266e58d1fbc8730b795a4916cbddf4cef6d1
x <y>
2024-04-28 20:54:00 +0200

    fixed tests due to design changes

--- test/operations_test.bat
+++ test/operations_test.bat
@@ -6,19 +6,19 @@ setup() {
     SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
     ROOT_PATH=$(cd "." && pwd)
 
-    # Create temporary files for testing
-    touch "$ROOT_PATH/env/test_env_1"
-    touch "$ROOT_PATH/env/test_env_2"
-    touch "$ROOT_PATH/env/test_env_3"
+    # Create temporary environments for testing
+    mkdir -p "$ROOT_PATH/.env/test_env_1"
+    mkdir -p "$ROOT_PATH/.env/test_env_2"
+    mkdir -p "$ROOT_PATH/.env/test_env_3"
 }
 
 # Cleanup function
 teardown() {
     # Clean up temporary files and directories
-    rm "$ROOT_PATH/env/test_env_1"
-    rm "$ROOT_PATH/env/test_env_2"
-    if [ -e "$ROOT_PATH/env/test_env_3" ]; then
-        rm "$ROOT_PATH/env/test_env_3"
+    rm -rf "$ROOT_PATH/.env/test_env_1"
+    rm -rf"$ROOT_PATH/.env/test_env_2"
+    if [ -e "$ROOT_PATH/.env/test_env_3" ]; then
+        rm -rf "$ROOT_PATH/.env/test_env_3"
     fi
 }
 
